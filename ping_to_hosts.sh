4b44c6081137f3c359384cf21231c39cd8ca889e
muse2023 <pandasuperfans@gmail.com>
2025-01-18 05:13:10 +0800

    改了一下

--- ping_to_hosts.sh
+++ ping_to_hosts.sh
@@ -7,21 +7,18 @@ target="www.gstatic.com"
 temp_file="/tmp/ping_results.txt"
 
 # 执行ping命令并存储结果
-ping -c 5 $target | grep 'bytes from' | awk -F' ' '{print $4 " " $7}' | sed 's/time=//g' > $temp_file
+ping -c 5 -4 $target | grep 'bytes from' | awk -F' ' '{print $4 " " $7}' | sed 's/time=//g' > $temp_file
 
 # 查找最小延迟的行
 min_delay_line=$(sort -k2 -n $temp_file | head -n 1)
 
 # 获取最低延迟的 IP 地址
-min_ip=$(echo $min_delay_line | awk '{print $1}')
-
-# 获取主机名
-hostname=$(echo $target | awk -F'.' '{print $1}')
+a=$(echo $min_delay_line | awk '{print $1}')
 
 # 写入/etc/hosts
-echo "$min_ip $hostname" | sudo tee -a /etc/hosts
+echo "$a $target" | sudo tee -a /etc/hosts
 
 # 删除临时文件
 rm $temp_file
 
-echo "IP地址 $min_ip 已经写入 /etc/hosts 作为 $hostname"
+echo "IP地址 $a 已经写入 /etc/hosts 作为 $target"
