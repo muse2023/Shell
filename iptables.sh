a4baff49e86a9e79b29ca70e89c3b7b2e659f93a
muse <130621848+muse2023@users.noreply.github.com>
2025-01-20 14:45:20 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -2,7 +2,7 @@
 
 
 iptables -t filter -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp -m multiport --dports 443,80,1024,5555,60002 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -m multiport --dports 443,80,1024,5555,8008,60002 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp -j DROP
 
 
